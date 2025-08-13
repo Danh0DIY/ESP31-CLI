4c736ae2ba2414c9e6ba37bcdff3bf5459d781f4
Danh0DIY <dnn345169@gmail.com>
2025-08-13 13:41:32 +0700

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,13 @@
+// Chân LED
+int ledPin = 8;
 
+void setup() {
+  pinMode(ledPin, OUTPUT); // Đặt chân 8 làm OUTPUT
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Bật LED
+  delay(500);                 // Đợi 0.5 giây
+  digitalWrite(ledPin, LOW);  // Tắt LED
+  delay(500);                 // Đợi 0.5 giây
+}
